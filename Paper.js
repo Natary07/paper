556b2232bd3e8f33ac13a107bf143edd4e31c79e
Natary07 <71980983+Natary07@users.noreply.github.com>
2021-02-10 15:58:26 -0600

    Update Paper.js

--- Paper.js
+++ Paper.js
@@ -6,7 +6,7 @@ class Paper {
           friction: 0.5,
           density: 0.1
       }
-      this.body = Bodies.circle(x, y, 50, options);
+      this.body = Bodies.circle(x, y, 30, options);
       
       
       World.add(world, this.body);
@@ -17,7 +17,7 @@ class Paper {
       translate(pos.x, pos.y);
       ellipseMode(CENTER);
       fill("green");
-      ellipse(0,0, 50, 50);
+      ellipse(0,0, 30, 30);
       pop();
 
     }
