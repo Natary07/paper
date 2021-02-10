964f5a04b549026d248db2f0d89747942ac2d350
Natary07 <aryanpat2007@gmail.com>
2021-02-10 16:08:04 -0600

    density

--- Paper.js
+++ Paper.js
@@ -4,9 +4,9 @@ class Paper {
           isStatic: false,
           restitution: 0.3,
           friction: 0.5,
-          density: 0.1
+          density: 0.7
       }
-      this.body = Bodies.circle(x, y, 30, options);
+      this.body = Bodies.circle(x, y, 50, options);
       
       
       World.add(world, this.body);
@@ -17,7 +17,7 @@ class Paper {
       translate(pos.x, pos.y);
       ellipseMode(CENTER);
       fill("green");
-      ellipse(0,0, 30, 30);
+      ellipse(0,0, 50, 50);
       pop();
 
     }
